c2a0a9391408b4b2afaffac3ee62e4540951f6d2
Pierre Le Fevre <hello@pierrelf.com>
2023-08-14 11:45:12 +0200

    debug - print time to fetch each subsystem

--- worker.py
+++ worker.py
@@ -6,29 +6,37 @@ import pymongo
 
 
 def get_db_latest():
+    timer = time.time()
     client = pymongo.MongoClient(env.read_env("MONGO_URL"))
     db = client["deploy"]
     collection = db["jobs"]
     jobs = collection.find({"$nor": [{"type": {"$regex": "repair"}}, {"status": {"$regex": "failed"}}]}).sort(
         "createdAt", -1).limit(10)
+    print("Time to fetch db: " + str(time.time() - timer))
     return list(jobs)
 
 
 def get_status():
+    timer = time.time()
     response = requests.get(
         f'{env.read_env("API_URL")}/landing/v2/status?n=100')
+    print("Time to fetch status: " + str(time.time() - timer))
     return response.json()
 
 
 def get_capacities():
+    timer = time.time()
     response = requests.get(
         f'{env.read_env("API_URL")}/landing/v2/capacities?n=1')
+    print("Time to fetch capacities: " + str(time.time() - timer))
     return response.json()
 
 
 def get_stats():
+    timer = time.time()
     response = requests.get(
         f'{env.read_env("API_URL")}/landing/v2/stats?n=1')
+    print("Time to fetch stats: " + str(time.time() - timer))
     return response.json()
 
 
