259ba04006975d78d405e5d4322d89a9cc4daf59
Pierre Le Fevre <hello@pierrelf.com>
2023-08-24 16:18:33 +0200

    Cleanup debug

--- worker.py
+++ worker.py
@@ -6,40 +6,32 @@ import pymongo
 import logger
 
 def get_db_latest():
-    timer = time.time()
     client = pymongo.MongoClient(env.read_env("MONGO_URL"))
     db = client["deploy"]
     collection = db["jobs"]
     # all the jobs that are not repair or failed or pending, sorted by createdAt, descending, limit 10
     jobs = collection.find({"$nor": [{"type": {"$regex": "repair"}}, {"status": {"$regex": "failed"}}, {"status": {"$regex": "terminated"}}, {"status": {"$regex": "pending"}}]}).sort(
         "createdAt", -1).limit(10)
-    logger.log("Time to fetch db: " + str(time.time() - timer))
     jobsList = list(jobs)
     client.close()
     return list(jobsList)
 
 
 def get_status():
-    timer = time.time()
     response = requests.get(
         f'{env.read_env("API_URL")}/landing/v2/status?n=100')
-    logger.log("Time to fetch status: " + str(time.time() - timer))
     return response.json()
 
 
 def get_capacities():
-    timer = time.time()
     response = requests.get(
         f'{env.read_env("API_URL")}/landing/v2/capacities?n=1')
-    logger.log("Time to fetch capacities: " + str(time.time() - timer))
     return response.json()
 
 
 def get_stats():
-    timer = time.time()
     response = requests.get(
         f'{env.read_env("API_URL")}/landing/v2/stats?n=1')
-    logger.log("Time to fetch stats: " + str(time.time() - timer))
     return response.json()
 
 
